592a726134b34f22d54170e6f9a674607e284eb2
junior sousa <92957053+juniors0101@users.noreply.github.com>
2025-06-16 16:56:38 -0300

    criação de demais components
    criação de demais components
    Footer
    Header
    PageLayout
    Criação da página não encontrada NotFound
    Atualização no arquivo Path

--- src/Routes/Paths.jsx
+++ src/Routes/Paths.jsx
@@ -2,21 +2,26 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "../Pages/HomePage";
 import ProductListingPage from "../Pages/ProductListingPage";
 import ProductViewPage from "../Pages/ProductViewPage";
-import Header from "../components/Header";
+import PageLayout from "../layouts/PageLayout";
+import NotFound from "../Pages/NotFound";
 
 const Paths = () => {
-    return ( 
+    return (
         <>
-        <BrowserRouter>
-        <Header />
-        <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/productlistingpage" element={<ProductListingPage />} />
-            <Route path="/productviewpage" element={<ProductViewPage />} />
-        </Routes>
-        </BrowserRouter>
+
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<PageLayout />}>
+                        <Route  element={<HomePage />} />
+                        <Route path="/productlistingpage" element={<ProductListingPage />} />
+                        <Route path="/productviewpage" element={<ProductViewPage />} />
+                    </Route>
+                    <Route path="*" element={<NotFound />}/>
+                </Routes>
+
+            </BrowserRouter>
         </>
-     );
+    );
 }
- 
+
 export default Paths;
