73a1efc7d8a820d5fbf1d5c1f62bd4bee2ca3558
junior sousa <92957053+juniors0101@users.noreply.github.com>
2025-06-30 22:50:30 -0300

    teste

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -74,7 +74,7 @@ const HomePage = () => {
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
               minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-              ex ea commodo consequat.
+              ex ea commodo consequats.
             </p>
             <button className={styles.collectorEditionButton}>Ver coleção</button>
           </div>
